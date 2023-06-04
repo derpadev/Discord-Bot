35a05816f206ba09a40b09c5cb1e9680c99ac259
Devin Hua <115900310+derpadev@users.noreply.github.com>
2023-06-03 20:20:18 -0700

    Update apikeys.py

--- apikeys.py
+++ apikeys.py
@@ -1,2 +1,3 @@
+#discord bot token, unique to all bots
 #this is a random token I made up, NOT MINE. This is an example of what yours might look like.
 TOKEN = "AMSFKNAT5352ajsfnbakMAFKANK5359aakfsNLA"
